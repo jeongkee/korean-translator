cc25c3fa53d591db901a8df60f4653fd651d4686
jeongkee <GitHub   >
2025-02-21 10:30:16 +0900

    Simplify app for testing

--- app.py
+++ app.py
@@ -103,6 +103,13 @@ def translate_text(translator, text):
         return text, text
 
 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=8000)
